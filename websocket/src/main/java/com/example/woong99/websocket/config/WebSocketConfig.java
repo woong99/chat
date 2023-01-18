84069c0a6fa4f4a071aa29d1db604be216b86741
woong99 <dndry1072@naver.com>
2023-01-18 17:47:47 +0900

    websocket

---
+++ websocket/src/main/java/com/example/woong99/websocket/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.example.woong99.websocket.config;
+
+import com.example.woong99.websocket.controller.ChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "ws/chat").setAllowedOrigins("*");
+    }
+}
---
+++ websocket/src/main/java/com/example/woong99/websocket/dto/UserRequestDto.java
@@ -0,0 +1,8 @@
+package com.example.woong99.websocket.dto;
+
+import lombok.Data;
+
+@Data
+public class UserRequestDto {
+    private String nickname;
+}
